d06d07809c169089c01a9d54b6c7d075befc630a
natal <svetnata74@mail.ru>
2024-01-17 13:56:34 +0100

    jhjhj

--- conftest.py
+++ conftest.py
@@ -9,9 +9,7 @@ from utils import attach
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-    browser.config.base_url = 'https://demoqa.com'
+
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -28,16 +26,10 @@ def setup_browser(request):
     )
 
 
-    driver = webdriver.Remote(
-        command_executor=f'https://user1:1234@selenoid.autotests.cloud/wd/hub',
-        options=options,
-    )
-
     browser.config.driver = driver
-    # browser.config.base_url = 'https://online.metro-cc.ru/'
-    browser.config.timeout = 10
     browser.config.window_width = 1920
     browser.config.window_height = 1080
+    browser.config.base_url = 'https://demoqa.com'
 
     yield
 
