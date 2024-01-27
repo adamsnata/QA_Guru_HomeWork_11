0188d77233488c22d26365d641086474e949deee
natal <svetnata74@mail.ru>
2024-01-27 20:15:00 +0100

    jhjhj

--- conftest.py
+++ conftest.py
@@ -7,35 +7,54 @@ from utils import attach
 
 
 
+import pytest
+from selene import browser
+from selenium import webdriver
+
+
 @pytest.fixture(scope='function', autouse=True)
-def setup_browser(request):
-
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
-
-
-    browser.config.driver = driver
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
+def open_browser():
     browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_width = 1080
+    browser.config.window_height = 720
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = 'eager'
+    browser.config.driver_options = driver_options
 
     yield
 
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_html(browser)
-    attach.add_video(browser)
-
     browser.quit()
+
+
+# @pytest.fixture(scope='function', autouse=True)
+# def setup_browser(request):
+#
+#     options = Options()
+#     selenoid_capabilities = {
+#         "browserName": "chrome",
+#         "browserVersion": "100.0",
+#         "selenoid:options": {
+#             "enableVNC": True,
+#             "enableVideo": True
+#         }
+#     }
+#     options.capabilities.update(selenoid_capabilities)
+#     driver = webdriver.Remote(
+#         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+#         options=options
+#     )
+#
+#
+#     browser.config.driver = driver
+#     browser.config.window_width = 1920
+#     browser.config.window_height = 1080
+#     browser.config.base_url = 'https://demoqa.com'
+#
+#     yield
+#
+#     attach.add_screenshot(browser)
+#     attach.add_logs(browser)
+#     attach.add_html(browser)
+#     attach.add_video(browser)
+#
+#     browser.quit()
